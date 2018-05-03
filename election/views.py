d4ee08785e3c8cbce9509ad4f4de91693df0d47b
allenazamoras <allenazamoras@gmail.com>
2018-05-03 23:13:35 +0800

    Reversed edited return Response, because errors @.@

--- election/views.py
+++ election/views.py
@@ -1,6 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponseRedirect
 from django.contrib.auth import authenticate, login
 
 from rest_framework import viewsets
+from rest_framework import mixins
 from rest_framework import generics
 from rest_framework import permissions
 from rest_framework.response import Response
@@ -9,6 +12,7 @@ from election.models import Party, User
 from election.serializers import PartySerializer, UserSerializer
 from election.serializers import AppointSerializer
 from election.serializers import LoginSerializer
+from election.permissions import Appoint
 
 
 class PartyViewSet(viewsets.ModelViewSet):
@@ -30,16 +34,13 @@ class UserViewSet(viewsets.ModelViewSet):
     serializer_class = UserSerializer
 
     def create(self, request):
-        obj = self.get_object()
         req = request.POST
         user = User.objects.create_user(username=req['username'],
                                         password=req['password'],
                                         firstname=req['firstname'],
                                         lastname=req['lastname'])
         user.save()
-        serializer = UserSerializer(obj)
-        ret = {'Success': serializer.data}
-        return Response(ret)
+        return Response(True)
 
 
 class LoginView(generics.ListCreateAPIView):
@@ -48,17 +49,16 @@ class LoginView(generics.ListCreateAPIView):
     serializer_class = LoginSerializer
 
     def create(self, request):
-        obj = self.get_object()
+        print(request.data['username'])
+        print(request.data['password'])
         user = authenticate(username=request.data['username'],
                             password=request.data['password'])
         print(user)
-        ret = {'Error': 'Invalid Login username/password'}
         if user is not None:
             if user.is_active:
                 login(request, user)
-                serializer = UserSerializer(obj)
-                ret = {'Success': serializer.data}
-        return Response(ret)
+                return Response(True)
+        return Response(False)
 
 
 class AppointViewSet(viewsets.ModelViewSet):
